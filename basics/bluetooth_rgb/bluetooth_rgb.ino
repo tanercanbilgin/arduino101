3789de7a336b65b874981892b80086f14e84f75d
tanercanbilgin <tanercanbilgin@gmail.com>
2023-08-27 19:01:18 +0300

    updated the code for github

--- basics/bluetooth_rgb/bluetooth_rgb.ino
+++ basics/bluetooth_rgb/bluetooth_rgb.ino
@@ -1,34 +1,39 @@
-int data;
+char data = ' ';
 int red = 9;
 int green = 10;
 int blue = 11;
 
-
-void setup() {
+void setup()
+{
   Serial.begin(9600);
   pinMode(red, OUTPUT);
   pinMode(green, OUTPUT);
   pinMode(blue, OUTPUT);
-
 }
 
-void loop() {
-  if(Serial.available()) {
+void loop()
+{
+  if (Serial.available())
+  {
     data = Serial.read();
   }
 
-  if(data == 'r') {
+  if (data == 'r')
+  {
     digitalWrite(red, HIGH);
     digitalWrite(green, LOW);
     digitalWrite(blue, LOW);
-  } else if (data == 'g') {
+  }
+  else if (data == 'g')
+  {
     digitalWrite(red, LOW);
     digitalWrite(green, HIGH);
     digitalWrite(blue, LOW);
-  } else if (data == 'b') {
+  }
+  else if (data == 'b')
+  {
     digitalWrite(red, LOW);
     digitalWrite(green, LOW);
     digitalWrite(blue, HIGH);
   }
-
 }
